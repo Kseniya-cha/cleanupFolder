5001bdcf23fe5c4b321479313e65d4b9042acaed
Ksenia-cha <kseniyachapygova@gmail.com>
2023-04-25 09:36:30 +0300

    add logging

--- main.go
+++ main.go
@@ -2,34 +2,37 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"time"
 
 	"github.com/Kseniya-cha/cleanupFolder/pkg/config"
+	"github.com/Kseniya-cha/cleanupFolder/pkg/logger"
 )
 
 func main() {
 	// Чтение конфигурационного файла
 	cfg, err := config.GetConfig()
 	if err != nil {
-		log.Println(err)
+		fmt.Println(err)
+		return
 	}
+
+	log := logger.NewLogger(cfg)
 
 	for {
 		time.Sleep(cfg.TimeClean)
 		trashs, err := os.ReadDir(cfg.FilePath)
 		if err != nil {
-			log.Println(err)
+			log.Error(err.Error())
 		}
 
 		for _, trash := range trashs {
 			fmt.Println(trash.Name())
 			err = os.Remove(cfg.FilePath + trash.Name())
 			if err != nil {
-				log.Println(err)
+				log.Error(err.Error())
 			}
 		}
-		log.Println("clean trash!")
+		log.Info("clean trash!")
 	}
 }
